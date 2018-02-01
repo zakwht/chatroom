a679fe27fdaef887e55265eeb7833f251afdd81d
Zak <35615058+zakwht@users.noreply.github.com>
2018-02-01 12:18:55 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ app.use(express.static('client'));
 
 var io = require('socket.io')(server);
 
+var saved = [];
+
 io.on('connection', function (socket) {
   socket.on('message', function (msg) {
+    saved.push(msg);
     io.emit('message', msg);
   });
+  for(var i = 0; i < saved.length; i++) {
+   socket.emit('message', saved[i]);
+ }
 });
 
 server.listen(8080, function() {
